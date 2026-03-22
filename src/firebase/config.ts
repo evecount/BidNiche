97ec5c1d6e67b0063c3035e902759e8c117247b7
Gwen Lim <gwenlim1985@gmail.com>
2026-03-22 07:51:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1471106617-950ed",
+  "appId": "1:738875195565:web:ad1b0cc29163401a208624",
+  "apiKey": "AIzaSyBXxc3y1ovuCPArZ2SwIALuDGLYNnfNV_U",
+  "authDomain": "studio-1471106617-950ed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "738875195565"
+};
